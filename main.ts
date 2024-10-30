faadc7b09dfe957bc9f524f68f56f3253433a7d6
ella-an2 <ella.an@mths.ca>
2024-10-30 11:21:41 -0400

    tried to  code

--- main.ts
+++ main.ts
@@ -1,8 +1,29 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Ella An
+ * Created on: Oct 2024
+ * This program knows when to stop and go
 */
 
-basic.showString('Hello, World!')
+// variables
+let distanceToObject: number = 0
+
+// setup
+basic.showIcon(IconNames.Happy)
+basic.pause(500)
+
+distanceToObject = sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+)
+
+// make the wheel to move
+while (distanceToObject >= 10) {
+    basic.pause(500)
+    robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B0)
+    robotbit.StepperTurn(robotbit.Steppers.M2, robotbit.Turns.T1B0)
+    basic.pause(500) 
+
+}
+
