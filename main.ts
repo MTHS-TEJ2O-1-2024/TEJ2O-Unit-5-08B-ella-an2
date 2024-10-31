9fe3b38bcb8cd78c5ed894729199a57f2bf69bc9
ella-an2 <ella.an@mths.ca>
2024-10-30 20:29:38 -0400

    tried to code

--- main.ts
+++ main.ts
@@ -6,24 +6,30 @@
 */
 
 // variables
-let distanceToObject: number = 0
+let distanceToObject: number
 
 // setup
 basic.showIcon(IconNames.Happy)
 basic.pause(500)
 
+// to get the distance
 distanceToObject = sonar.ping(
     DigitalPin.P1,
     DigitalPin.P2,
     PingUnit.Centimeters
 )
+basic.showString((distanceToObject).toString())
 
 // make the wheel to move
 while (distanceToObject >= 10) {
+    robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B4)
     basic.pause(500)
-    robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B0)
-    robotbit.StepperTurn(robotbit.Steppers.M2, robotbit.Turns.T1B0)
-    basic.pause(500) 
+    robotbit.StepperTurn(robotbit.Steppers.M2, robotbit.Turns.T1B4)
 
+    // to get the distance again
+    distanceToObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
 }
-
